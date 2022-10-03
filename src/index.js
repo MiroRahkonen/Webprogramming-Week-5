5303c8da54e078719eeefc24a12747802e2a05e1
MiroRahkonen <80097068+MiroRahkonen@users.noreply.github.com>
2022-10-03 10:13:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 const fetchData = async () => {
-  const url =
-    "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
-  const res = await fetch(url);
-  const data = await res.json();
-
-  initMap(data);
-};
-
-const initMap = (data) => {
-  let map = L.map("map").setView([65.05, 28.1], 5);
-  let geoJson = L.geoJson(data, {
-    onEachFeature: getFeature,
-    style: styling
-  }).addTo(map);
-
-  let osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 10,
-    minZoom: -3
-  }).addTo(map);
-  map.fitBounds(geoJson.getBounds());
-};
-
-var styling = {
-  weight: 2
-};
-
-const getFeature = (feature, layer) => {
-  if (!feature.properties.nimi) return;
-  const name = feature.properties.nimi;
-  console.log(name);
-  layer.bindPopup(
-    `<ul>
-          <li>Name: Test</li>
-          <li>Year: 2000</li>
-      </ul>`
-  );
-};
-
-fetchData();
+    const url =
+      "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+    const res = await fetch(url);
+    const data = await res.json();
+    initMap(data);
+    console.log(data);
+  };
+  
+  const initMap = (data) => {
+    let map = L.map("map").setView([65.05, 28.1], 5);
+    let geoJson = L.geoJson(data, {
+      onEachFeature: getFeature,
+      style: styling
+    }).addTo(map);
+  
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      minZoom: 10
+    }).addTo(map);
+    map.fitBounds(geoJson.getBounds());
+  };
+  
+  var styling = {
+    minZoom: -3,
+    weight: 2
+  };
+  
+  const getFeature = (feature, layer) => {
+    if (!feature.properties.nimi) return;
+    const name = feature.properties.nimi;
+    const year = feature.properties.vuosi;
+    layer.bindPopup(
+      `<ul>
+            <li>Name: ${name}</li>
+            <li>Year: ${year}</li>
+        </ul>`
+    );
+  };
+  
+  fetchData();
+  
